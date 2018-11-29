b639e1d50bec49bb563debfef4e6180229b33cc0
01212 <marlonwiprud1@gmail.com>
2018-11-28 20:33:36 -0500

    pop-up working with no errors'

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hi')
+// const viewPort = document.getElementById("viewport");
+// const logo = document.getElementById('hplogo')
+
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+inputElement.addEventListener('change', (e) => {
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+imgElement.onload = function() {
+  let mat = cv.imread(imgElement);
+  cv.imshow('canvasOutput', mat);
+  mat.delete();
+};
+
+function onOpenCvReady() {
+  document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+}
+document.getElementById('body').addEventListener("load", onOpenCvReady);
